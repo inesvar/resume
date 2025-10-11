737b7261e470c464ed0ee77a7f8db17dd595f07f
inesvar <90340779+inesvar@users.noreply.github.com>
2025-10-11 16:37:13 +0200

    Premières adaptations

--- src/app/resumeData/you.ts
+++ src/app/resumeData/you.ts
@@ -10,13 +10,13 @@ export type You = {
 };
 
 export const you: You = {
-  name: "Barthélemy Paléologue",
-  phoneNumber: "+33 6 37 29 67 37",
-  email: "barth@paleologue.fr",
-  onlinePortfolio: "https://barth.paleologue.fr",
-  location: "Palaiseau, France",
+  name: "Inès Varhol",
+  phoneNumber: "+33 6 28 50 47 80",
+  email: "ines.varhol@proton.me",
+  onlinePortfolio: "https://github.com/inesvar",
+  location: "Angers, France",
   lookingFor: S({
-    fr: "A la recherche d'un poste d'ingénieur en informatique graphique",
-    en: "Looking for a position as a computer graphics engineer",
+    fr: "A la recherche d'un poste de développeur logiciel Rust",
+    en: "Looking for a position as a Rust software developer",
   }),
 };
